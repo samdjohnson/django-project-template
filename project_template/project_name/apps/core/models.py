61b0442672c23a872d5b0f76db4175d0f9e6c53d
Sam Johnson <sjohns52@gmail.com>
2014-06-18 00:28:02 -0500

    new name for core date time model

--- project_template/project_name/apps/core/models.py
+++ project_template/project_name/apps/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Created(models.Model):
+class DatetimeAware(models.Model):
     created = models.DateField(auto_now_add=True)
     modified = models.DateField(auto_now=True)
 
