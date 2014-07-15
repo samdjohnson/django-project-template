fec2f444efd140941b412b78472821e1224dfdaf
Sam Johnson <sjohns52@gmail.com>
2014-07-15 17:36:17 -0500

    added core views that can be extended by apps and js utilities I've used

--- project_name/apps/core/static/core/js/core.js
+++ project_name/apps/core/static/core/js/core.js
@@ -27,4 +27,166 @@ $.ajaxSetup({
         }
     }
 });
+
+/************************************************
+*
+* Jquery Plugin to better serialize form data
+* into a dictionary.
+*
+* Usage:
+*
+* HTML: 
+*
+* <form id="basic-form">
+*   <input type="text" name="name" placeholder="Your Name"/>
+*   <input type="text" name="email" placeholder="you@email.com"/>
+* </form>
+*
+* JS:
+*
+* $("#basic-form").serializeForm() -> { "name": "<name>", "email": "<email>" }
+*
+************************************************/
+
+(function( $ ){
+    $.fn.serializeForm = function( options ){
+        var arr = $(this).serializeArray( options );
+        var d = {};
+        var counter = {};
+        for( var i in arr ){
+            var name = arr[i]['name'];
+            var value = arr[i]['value'];
+
+            // handle multiple keys with the same name
+            if( counter[name] ){
+                name = name + "_" + ( counter[name]++ );
+            } else {
+                counter[name] = 1;
+            }
+            d[name] = value;
+        }
+        return d;
+    }
+})( jQuery );
+
+/******************************************
+*
+*
+* Ajax Inline Updating
+*
+* Requires Bootstrap
+* 
+* Usage:
+* 
+* <input type="text impressions" data-ajax-inline='true'>
+*
+*
+* $(".impresions").ajaxInline({
+*   "url": "/adproducts/product-members/",
+*   "dataId": "data-product-member-id", (optional)
+*   "attr": "impressions", (optional),
+*   "scrub": function() (optional)
+* })
+*
+*
+*******************************************/
+
+(function( $ ){
+    var Lock = (function(){
+        var keys = {};
+
+        function lock( key ){
+            keys[key] = "locked";
+        }
+
+        function unlock( key ){
+            delete keys[key];
+        }
+
+        function islocked( key ){
+            return keys[key] && ( keys[key] === "locked" );
+        }
+
+        var exports = {};
+        exports.lock = lock;
+        exports.unlock = unlock;
+        exports.islocked = islocked;
+        return exports;
+    })();
+
+    $.fn.ajaxInline = function( options ){
+        var selector = this.selector;
+
+        var settings = $.extend({
+            "url": "",
+            "dataId": "data-id",
+            "attr": selector.replace(/(\.|#)/g, ''),
+            "scrub": function(val){ return val; },
+            "format": function(val){ return val; }
+        }, options);
+
+        $(document).on("keypress", selector, function( e ){
+            if( e.keyCode === 13 ) $(this).focusout();
+        });
+
+        $(document).on("keyup", selector, function( e ){
+            var id = getId( this );
+            if( e.keyCode !== 13 ) Lock.lock( selector + id );
+        });
+
+        $(document).on("focusout", selector, function(){
+            var self = this;
+            var id = getId( this );
+            var newVal = settings.scrub( $(this).val() );
+
+            if( !Lock.islocked( selector + id ) ) return; 
+
+            var postData = {};
+            postData[settings.attr] = newVal;
+
+            $.ajax({
+                "url": settings.url + id,
+                "data": postData,
+                "type": "POST",
+                "success": function( data ) {
+                    if( data.error ) return error( self );
+                    Lock.unlock( selector + id );
+                    success( self );
+                    $(self).val( settings.format( newVal ) );
+                },
+                "error": function( data ) {
+                    error( self );
+                }
+            });
+        });
+
+        function success( self ){
+            setStatus( self, "<span class='badge badge-success'>Saved!</span>" );
+        }
+
+        function error( self ){
+            setStatus( self, "<span class='badge badge-important'>Something went wrong</span>" );
+        }
+
+        function setStatus( self, message ){
+            var container = getContainer( self );
+            var status = container.find(".status");
+            if( status.length === 0 ) return;
+
+            status.html( message );
+            setTimeout(function(){
+                status.html("");
+            }, 3000)
+        };
+
+
+        function getId( self ){
+            return getContainer( self ).attr( settings.dataId );
+        } 
+
+        function getContainer( self ){
+            return $(self).closest( "[" + settings.dataId + "]" );
+        }
+    }
+})( jQuery );
     
--- project_name/apps/core/views.py
+++ project_name/apps/core/views.py
@@ -1,1 +1,46 @@
+import json
+
+from django.contrib.admin.views.decorators import staff_member_required, login_required
+from django.http import HttpResponse, Http404
 from django.views.generic import View
+
+def _json_response(context):
+    return HttpResponse(json.dumps(context), mimetype="application/json")
+
+class LoginRequiredView(View):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredView, self).dispatch(request, *args, **kwargs)
+
+class LoginTemplateView(LoginRequiredView, TemplateView):
+    pass
+
+class StaffMemberRequiredView(View):
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StaffMemberRequiredView, self).dispatch(request, *args, **kwargs)
+
+class StaffTemplateView(StaffMemberRequiredView, TemplateView):
+    pass
+
+class AJAXUpdateView(DetailView):
+    def get(self, request, *args, **kwargs):
+        json_obj = serializers.serialize('json', [self.get_object(), ])
+        return HttpResponse(json_obj, mimetype="application/json")
+
+    def post(self, request, *args, **kwargs):
+        self._update_object(request.POST)
+        return self.render_to_response({"success": "It worked!"})
+
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(json.dumps(context), mimetype="application/json")
+
+    def _update_object(self, post):
+        print post
+        print self.model._meta.fields
+        obj = self.get_object()
+        for field in self.model._meta.fields:
+            if post.get(field.name):
+                val = field.get_prep_value(post.get(field.name))
+                setattr(obj, field.get_attname(), val)
+        obj.save()
