03fd95ea44b9750ef2ed6d929b5cba99c4e7cd15
Sam Johnson <sjohns52@gmail.com>
2014-07-15 18:08:09 -0500

    added extra js and css blocks to base.html and imported login_required from the correct module

--- project_name/apps/core/views.py
+++ project_name/apps/core/views.py
@@ -1,6 +1,7 @@
 import json
 
-from django.contrib.admin.views.decorators import staff_member_required, login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404
 from django.views.generic import View
 
